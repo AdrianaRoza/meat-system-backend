adf7db61e46f8725a85d7b14764154de73943be8
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-07-17 16:30:21 -0400

    making connection with the frontend

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import meatRouter
 
 app = FastAPI(title="Meat-system")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Inclui as rotas da aplicação
 app.include_router(meatRouter.router)
