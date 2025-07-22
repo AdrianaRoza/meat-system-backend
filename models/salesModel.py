d181f3e3e7450c3d48108d6ae50f89f4574159e2
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-07-22 09:32:29 -0400

    Create new table structure

---
+++ models/salesModel.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Sale(Base):
+    __tablename__ = "sales"
+
+    id = Column(Integer, primary_key=True, index=True)
+    meat_id = Column(Integer, ForeignKey("meats.id"), nullable=False)
+    quantity_kg = Column(Float, nullable=False)
+    total_price = Column(Float, nullable=False)
+
+    # Relacionamento (opcional, para facilitar consultas)
+    meat = relationship("Meat")
