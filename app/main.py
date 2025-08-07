7b3cc146cc72754e80b98f6c2d1a44eb91f5c89e
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-07 10:58:28 -0400

    include routers no main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from .database import Base, engine
 from .models import models
+from .routers import meat
+
 
 app = FastAPI()
 
 # Cria as tabelas se ainda não existirem
 Base.metadata.create_all(bind=engine)
+
+# Adiciona as rotas
+app.include_router(meat.router)
 
 @app.get("/")
 def home():
