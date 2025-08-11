b8489abc2de43843faa6b27b443185f6e7bba841
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-11 14:36:37 -0400

    include router routerRestock

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
 from .database import Base, engine
-from .models import MeatModels
 from .routers import RouterMeat
 from .routers import RouterSale
+from .routers import RouterRestock
 
 
 app = FastAPI()
-
+        
 # Cria as tabelas se ainda não existirem
 Base.metadata.create_all(bind=engine)
 
 # Adiciona as rotas
 app.include_router(RouterMeat.router)
 app.include_router(RouterSale.router)
+app.include_router(RouterRestock.router)
 
 @app.get("/")
 def home():
