8ca474544994600076296102442c9151cb06b5c6
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-12 10:24:13 -0400

    refactor database

--- app/database.py
+++ app/database.py
@@ -3,25 +3,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-# Carrega as variáveis do .env
 load_dotenv()
-
-# Pega a string do banco
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Cria o engine de conexão
 engine = create_engine(DATABASE_URL)
 
-# Sessão do banco (para executar comandos)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-# Base para criar os modelos
 Base = declarative_base()
-
-# Função para fornecer a sessão para o FastAPI
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
