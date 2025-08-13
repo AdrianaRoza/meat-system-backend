bcdb2755cea1f6f711bfb32366c093215bf92521
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-08-13 08:56:08 -0400

    adding relaxations

--- app/schemas/SaleSchema.py
+++ app/schemas/SaleSchema.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
 
+class MeatInSale(BaseModel):
+    id: int
+    name: str
+    price_per_kg: float
+
+    class Config:
+        orm_mode = True
+
 class SaleBase(BaseModel):
     meat_id: int
     quantity_kg: float
@@ -10,6 +18,7 @@ class SaleCreate(SaleBase):
 class SaleResponse(SaleBase):
     id: int
     total_price: float
+    meat: MeatInSale  # adiciona a relação
 
     class Config:
         orm_mode = True
